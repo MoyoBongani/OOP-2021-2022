5aaa1d09ee260a17ae819e08797503aa71ae981d
Bongani Moyo <C20309081@mytudublin.ie>
2022-02-16 12:17:36 +0000

    2 loops created

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -2,53 +2,47 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class Loops extends PApplet
-{
-
+public class Loops extends PApplet {
 
 	int mode = 0;
 
-	public void settings()
-	{
+	public void settings() {
 		size(500, 500);
 	}
 
 	public void setup() {
 		colorMode(HSB);
-		
+
 	}
 
-	public void keyPressed()
-	{
-		if (key >= '0' && key <='9')
-		{
+	public void keyPressed() {
+		if (key >= '0' && key <= '9') {
 			mode = key - '0';
 		}
 		println(mode);
 	}
 
-	float magicMap(float a, float b, float c, float d, float e)
-	{
+	float magicMap(float a, float b, float c, float d, float e) {
 		float output;
 		a -= b;
 		c -= b;
-		e-= d;
+		e -= d;
 
-		output = ((a/c)*e) + d;
+		output = ((a / c) * e) + d;
 
 		return output;
 	}
 
-	float magicMap1(float a, float b, float c, float d, float e)
-	{
-		float r1 = c -b;
-		float r2 = e-d;
+	float magicMap1(float a, float b, float c, float d, float e) {
+		float r1 = c - b;
+		float r2 = e - d;
 		float howFar = a - b;
 
 		return d + ((howFar / r1) * r2);
 	}
 
-	
+	float offset = 0;
+
 	public void draw()
 	{
 		
@@ -66,7 +60,17 @@ public class Loops extends PApplet
 				}
 				break;
 			case 1:
-			
+				background(0);
+				int squares = (int) (mouseX / 20.0f);
+				float h = width / (float) squares;
+				for (int i = 0; i < squares; i++)
+				{
+					noStroke();
+					fill(map(i, 0, squares, 0, 255), 255, 255);
+					float x = map(i, 0, squares, 0, width);
+					rect(x, x, h, h);
+					rect((width - h) - x, x, h, h);
+				}
 				break;
 				//map(a,b,c,d,e);
 				//a = inputvalue
@@ -74,6 +78,22 @@ public class Loops extends PApplet
 				// d, e 0 - start and and of the end range
 
 				// map(-2, 10, 90, 200, 233);
+			case 2:
+				background(0);
+				int circles = (int) (mouseX / 20.0f);
+				offset += mouseY;
+				float d = width / (float) circles;
+				for (int j = 0; j < circles; j++)
+				{
+					for(int i=0; i < circles; i++)
+					{
+						noStroke();
+						float c = map((i + j + offset) 0, circles * 2, 0, 255) % 256;
+						fill(c, 255, 255);
+						float x = map
+						circle(map(i, 0, circles - 1, d / 2.0f, width - (d / 2.0f)));
+					}
+				}
 
 		}
 	}
